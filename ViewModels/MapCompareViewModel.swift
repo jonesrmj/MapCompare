ef2f72772a9e0f5fc59fd3a3040a1b87aa9d62ed
jonesrmj <51058123+jonesrmj@users.noreply.github.com>
2020-08-01 11:06:37 -0400

    Update MapCompareViewModel.swift
    Display Current Location in Origin Text Field. Brought in code to handle requesting location and getting permission from the user.

--- ViewModels/MapCompareViewModel.swift
+++ ViewModels/MapCompareViewModel.swift
@@ -15,6 +15,7 @@ import MapKit
 class MapCompareViewModel: NSObject, ObservableObject, Identifiable {
     private let locationManager = CLLocationManager()
     private var currentRegion: MKCoordinateRegion?
+    private var currentLocation: MKCoordinateRegion?
     
     @Published var origin: String = ""
     @Published var destination: String = ""
@@ -23,6 +24,35 @@ class MapCompareViewModel: NSObject, ObservableObject, Identifiable {
         super.init()
         
         locationManager.delegate = self
+        
+        attemptLocationAccess()
+    }
+    
+    private func attemptLocationAccess() {
+        // 1 (Check to see if user enabled location services)
+        guard CLLocationManager.locationServicesEnabled() else {
+            return
+        }
+        // 2 (When finding the location’s coordinates, it’s not the most precise, so an accuracy of 100 meters is more than adequate)
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        // 3 (This informs the app when the privacy setting is changed and your location is updated)
+        locationManager.delegate = self
+        // 4 (If the user enabled location services, then get current location)
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        } else {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if (status == CLAuthorizationStatus.denied) {
+            // The user denied authorization
+        } else if (status == CLAuthorizationStatus.authorizedAlways
+            || status == CLAuthorizationStatus.authorizedWhenInUse) {
+            locationManager.requestLocation()
+            // The user accepted authorization
+        }
     }
     
     func setOrigin() {
@@ -31,47 +61,47 @@ class MapCompareViewModel: NSObject, ObservableObject, Identifiable {
 }
 
 extension MapCompareViewModel: CLLocationManagerDelegate {
-  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-    // 1 (Ensure the user has given the app authorization to access location information)
-    guard status == .authorizedWhenInUse else {
-        return
-    }
-    manager.requestLocation()
-  }
-
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    guard let firstLocation = locations.first else {
-        return
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // 1 (Ensure the user has given the app authorization to access location information)
+        guard status == .authorizedWhenInUse else {
+            return
+        }
+        manager.requestLocation()
     }
     
-    //1 (commonDelta refers to zoom level you want. Increase the value for more coverage)
-    let commonDelta: CLLocationDegrees = 25 / 111
-    let span = MKCoordinateSpan(
-        latitudeDelta: commonDelta,
-        longitudeDelta: commonDelta)
-    //2 (The  region you created using the coordinates obtained via CoreLocation's delegate)
-    let region = MKCoordinateRegion(center: firstLocation.coordinate, span: span)
-    currentRegion = region
-    
-    /*
-    // 2 (Returns an array of placemarks in its completion handler)
-    CLGeocoder().reverseGeocodeLocation(firstLocation) { places, _ in
-        // 3 (Make sure text field is empty).
-        guard
-            let firstPlace = places?.first,
-            self.originTextField.contents == nil
-            else {
-                return
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.first else {
+            return
         }
         
-        // 4 (Store the current location and update the field)
-        self.currentPlace = firstPlace
-        self.originTextField.text = firstPlace.abbreviation
-     */
+        //1 (commonDelta refers to zoom level you want. Increase the value for more coverage)
+        let commonDelta: CLLocationDegrees = 25 / 111
+        let span = MKCoordinateSpan(
+            latitudeDelta: commonDelta,
+            longitudeDelta: commonDelta)
+        //2 (The  region you created using the coordinates obtained via CoreLocation's delegate)
+        let region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        currentRegion = region
+        
+        
+        // 2 (Returns an array of placemarks in its completion handler)
+        CLGeocoder().reverseGeocodeLocation(currentLocation) { places, _ in
+            // 3 (Make sure text field is empty).
+            guard
+                let firstPlace = places?.first,
+                self.origin == ""
+                else {
+                    return
+            }
+            
+            // 4 (Store the current location and update the field)
+            //self.currentPlace = firstPlace
+            self.origin = firstPlace.name ?? "Current Location"
+        }
     }
-  }
-
-  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    print("Error requesting location: \(error.localizedDescription)")
-  }
-
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error requesting location: \(error.localizedDescription)")
+    }
+    
+}
